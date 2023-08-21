be9ee9b91b9569cede68899f5ed4ca1277a23e18
Dimentiy88 <bodartinov88@gmail.com>
2023-08-21 19:05:54 +0200

    commit end

--- src/js/markup.js
+++ src/js/markup.js
@@ -11,28 +11,17 @@ export function createMarkup(array) {
         downloads,
       }) =>
         `<div class="photo-card">
-<a href="${largeImageURL}" data-lightbox="gallery">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-</a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      ${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      ${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      ${downloads}
-    </p>
-  </div>
-</div>`
+            <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+              <p class="info-item"><b>Likes:</b>${likes}</p>
+              <p class="info-item"><b>Views:</b>${views}</p>
+              <p class="info-item"><b>Comments:</b>${comments}</p>
+              <p class="info-item"><b>Downloads:</b>${downloads}</p>
+            </div>
+          </div>`
     )
+
     .join(``);
 }
